b257847f9c7abe23a15bc1548f59a4fcfd17c4a2
Zeerating-032 <75830883+Zeerating-032@users.noreply.github.com>
2021-05-16 13:26:25 +0800

    Update PolyMulti_All.py

--- PolyMulti_All.py
+++ PolyMulti_All.py
@@ -22,6 +22,9 @@ class fraction:
 		if self.up%self.down==0:
 			return str(self.up//self.down)
 		else:
+			if self.down<0:
+				self.up*=-1
+				self.down*=-1
 			return f"({self.up}/{self.down})" #括起來比較好看
 
 class poly:
@@ -65,21 +68,21 @@ class poly:
 
 
 def sperate(que): #arg:str
+	corrForm=re.findall("[x\d+-/.^()]", que) #格式檢查
+	if len(corrForm)!=len(que):
+		print("格式錯誤")
+		exit(0)
+
 	#ex. 3x(x^2+1)(2-x)1/2
-	que=que.replace("-", "+-") #3x(x^2+1)(2+-x)1/2 #等等方便直接分離
-	each=re.compile("\(([\w+-/.^]+)\)")
-	all_eq=re.findall(each, que) #找出所有以括號包覆的, ["x^2+1", "2+-x"] 
-	
-	for i in all_eq:
-		que=que.replace(f"({i})", " ")
-	
-	all_eq.extend(que.split()) #加入未以括號包覆的, ["3x", "1/2"]
+	each=re.compile("[\w+-/.^]+")
+	all_eq=re.findall(each, que) #['3x', 'x^2+1', '2-x', '1/2']
 	
 	return all_eq 
 
 def to_p(all_eq): #arg:str list
-	r=[] #r用來存所有dic
+	r=[] #r用來存所有poly
 	for eq in all_eq:
+		eq=eq[0]+eq[1:].replace("-", "+-") #等等方便直接分離 #若括號後即負號則不變
 		li=eq.split("+") #["2", "-x"]
 		dic={}
 		for i in li:
@@ -88,7 +91,7 @@ def to_p(all_eq): #arg:str list
 				coe, times=a.group(1), a.group(2) #coe = coefficient
 				
 				if coe=="" or coe=="-": #have no coe or only negative sign 
-					coe+="1"		
+					coe+="1"
 				
 				coe=stof(coe)
 				times=int(times[1:]) if times is not None else 1 #去除前面的^
@@ -98,6 +101,23 @@ def to_p(all_eq): #arg:str list
 				
 		r.append(poly(dic)) #作完dict就轉成poly
 	return r
+
+
+def stof(num): #string to fraction #arg:str
+	if "/" in num: #a fraction
+		li=num.split("/")
+		a, b=int(li[0]), int(li[1])
+		
+	elif "." in num: #a decimal
+		li=num.split(".")
+		a=int(li[0]+li[1])
+		b=int("1"+"0"*len(li[1]))
+		a, b=reduction(a, b) #約分縮小數字
+
+	else: #an integet
+		a, b=int(num), 1
+	
+	return fraction(a, b)
 
 def reduction(a, b): #arg:two int
 	c, d=max(a, b), min(a, b)
@@ -109,31 +129,11 @@ def reduction(a, b): #arg:two int
 	#(a, b)=d
 	return a//d, b//d
 
-def stof(num): #string to fraction #arg:str
-	if "/" in num: #a fraction
-		li=num.split("/")
-		a, b=int(li[0]), int(li[1])
-		return fraction(a, b)
-		
-	elif "." in num: #a decimal
-		li=num.split(".")
-		a=int(li[0]+li[1])
-		b=int("1"+"0"*len(li[1]))
-		a, b=reduction(a, b) #約分縮小數字
-		return fraction(a, b)
-		
-	else: #an integet
-		return fraction(int(num), 1)
 
 def iter_mul(p_list): #arg:poly list
-	length=len(poly_li)
-	while length>=2:
-		p1, p2=p_list[0], p_list[1]
-		pp=p1*p2
-		
-		p_list.pop(0)
-		p_list[0]=pp
-		length-=1
+	pp=p_list[0]
+	for i in p_list[1:]:
+		pp*=i
 	return pp
 
 
